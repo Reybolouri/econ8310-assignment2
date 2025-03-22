24c3d117c95168751235bc3b35dfc5e29cd69e1d
RBolourui <160203017+RBolourui@users.noreply.github.com>
2025-03-22 13:50:49 -0500

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,64 +1,62 @@
 #Assignment2
 import pandas as pd
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+import xgboost as xgb
+
 
 train_url = "https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3.csv"
 test_url = "https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3test.csv"
-
-
-
 train_data = pd.read_csv(train_url)
 test_data = pd.read_csv(test_url)
 
-
 train_missing_values = train_data.isnull().sum()
-print("Train set missing values:\n", train_missing_values)
 test_missing_values = test_data.isnull().sum()
-print("Train set missing values:\n", test_missing_values)
 
-target = 'meal'
-features = train_data.columns.drop(target)
-X = train_data[features]
-y = train_data[target]
-
-X = pd.get_dummies(X)
-
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.3, random_state=42)
-
-model = RandomForestClassifier(random_state=42, n_estimators=100)
-
-modelFit_split = model.fit(X_train, y_train)
-
-# in sample/training accuracy
-train_preds = modelFit_split.predict(X_train)
-in_sample_accuracy = accuracy_score(y_train, train_preds)
-print("In sample (training) accuracy:", in_sample_accuracy)
-
-#out of sample/validation accuracy
-val_preds = modelFit_split.predict(X_val)
-out_of_sample_accuracy = accuracy_score(y_val, val_preds)
-print("Out of sample (validation) accuracy:", out_of_sample_accuracy)
-
-modelFit = model.fit(X, y)
+print("Train missing values:\n", train_missing_values)
+print("Test missing values:\n", test_missing_values)
 
 
-#---------------------
+#Drop non predictive columns
+train_data = train_data.drop(['id', 'DateTime'], axis=1)
 
-# Prep the test data.
+#Separate target variable meal from predictors
+X_train = train_data.drop('meal', axis=1)
+y_train = train_data['meal']
+#split the data
+train_X, val_X, train_Y, val_Y = train_test_split(X_train, y_train, test_size=0.33, random_state=42)
+
+
+xgb_model = xgb.XGBClassifier(random_state=42, use_label_encoder=False, eval_metric='logloss')
+
+# Fit the model on training set
+modelFit = xgb_model.fit(train_X, train_Y)
+
+# Evaluate model on training set 
+train_preds = modelFit.predict(train_X)
+train_accuracy = accuracy_score(train_Y, train_preds)
+print("Training Accuracy: {:.2f}%".format(100 * train_accuracy))
+
+# Evaluate model on validation set
+val_preds = modelFit.predict(val_X)
+val_accuracy = accuracy_score(val_Y, val_preds)
+print("Validation Accuracy: {:.2f}%".format(100 * val_accuracy))
+
+
+# Fit the Final Model on the Full Training Data
+final_model = xgb_model.fit(X_train, y_train)
+
+# -------process test data and make predictions
+test_data = test_data.drop(['id', 'DateTime'], axis=1)
 if 'meal' in test_data.columns:
-    X_test = test_data.drop(columns=['meal'])
-else:
-    X_test = test_data.copy()
+    test_data = test_data.drop('meal', axis=1)
 
-#Apply one hot encoding to test data
-X_test = pd.get_dummies(X_test)
-X_test = X_test.reindex(columns=X.columns, fill_value=0)
+X_test = test_data[X_train.columns]
 
 
-pred = modelFit.predict(X_test)
+predictions = final_model.predict(X_test)
+pred = predictions.astype(int).tolist()
 
 print("Number of predictions:", len(pred))
-print(pd.DataFrame({'predicted_meal': pred}).head())
+print("First 10 predictions:", pred[:10])
